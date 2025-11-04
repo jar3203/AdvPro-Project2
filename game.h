dde5d6014d62ebbcf12da32cf1f4a75248db9456
jar3203 <jar3203@rit.edu>
2025-11-04 18:44:14 -0500

    Completed Item, Enemy, Place
    Should be implemented properly, haven't run yet without player class implemented. I commented out some function overloading code because the main file never instantiates items or enemies without parameters as far as I'm aware, otherwise it would be needed. Also haven't verified the print statements match the output in result.txt, but it should be close if not exact for the required print statements thus far.

--- game.h
+++ game.h
@@ -10,11 +10,11 @@ private:
   string desc;
 public:
   Item(string n, string d);
-  Item(){name = "", desc = "";}
+  //Item(){name = "", desc = "";}
   string getName(void){return name;}
   string getDesc(void){return desc;}
 };
-Item::Item(string n, string d) : name(n), desc(d){}
+//Item::Item(string n, string d) : name(n), desc(d){}
 
 
 class Enemy {
@@ -24,19 +24,45 @@ private:
   int attackPower;
 public:
   Enemy(string n, int h, int a);
-  Enemy(){name = "", health = 0, attackPower = 0;}
-
+  //Enemy(){name = "", health = 0, attackPower = 0;}
   string getName(void){return name;}
   int getHealth(void){return health;}
   int getAttackPower(void){return attackPower;}
   void setHealth(int h){health -= h;}
 };
-Enemy::Enemy(string n, int h, int a) : name(n), health(h), attackPower(a){}
-
+//Enemy::Enemy(string n, int h, int a) : name(n), health(h), attackPower(a){}
 
 
 class Place {
-  /*TODO: write your code*/
+private:
+  string desc;
+public:
+  vector<Item> itemList;
+  vector<Enemy> enemyList;
+  vector<Place*> placeList;
+
+  Place(string desc);
+  string getDesc(){return desc;}
+  void addItem(const Item& i){itemList.push_back(i);}
+  void addEnemy(const Enemy& e){enemyList.push_back(e);}
+  void addPlace(Place* p){placeList.push_back(p);}
+  void printItems(){
+    if (itemList.empty()) {
+        cout << "- No items." << endl;
+    } else {
+        cout << "- Items:" << endl;
+        for (auto& Item : itemList) {
+            cout << "  - " << Item.getName() << ": " << Item.getDesc() << endl;
+        }}}
+  void printEnemies(){
+    if (enemyList.empty()) {
+        cout << "- No enemies." << endl;
+    } else {
+        cout << "- Enemies:" << endl;
+        for (auto& Enemy : enemyList) {
+            cout << "  - " << Enemy.getName() << " (Health: " << Enemy.getHealth() 
+            << ", Attack Power: " << Enemy.getAttackPower() << ")" << endl;
+        }}}
 };
 
 
